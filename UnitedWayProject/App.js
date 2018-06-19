1034980edd316ca985f6629eb2ba5eb370fc78ac
umakrishnan2 <uma.krishnan3998@gmail.com>
2018-06-19 13:19:34 -0500

    github issues

--- UnitedWayProject/App.js
+++ UnitedWayProject/App.js
@@ -1,56 +1,70 @@
 import React from 'react';
-import { Header, StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
+import { Header, StyleSheet, Text, View, Image, Alert, Button, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-
+  _onPressButton() {Alert.alert('Recording...')}
   render() {
     return (
       <View style={{justifyContent: 'space-between', height: 500}}>
 
-      <View style={{justifyContent: 'flex-end', alignSelf:'stretch', height: 50, backgroundColor: 'lightblue'}}>
+      <View style={{justifyContent: 'flex-end', width: 400, height: 50, backgroundColor: 'lightblue'}}>
       <Text style={{fontWeight:'bold', fontSize: 25}}>Record Interview Questions</Text>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 1</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source ={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 2</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 3</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 4</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 5</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 6</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+       <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
-      <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 7</Text>
-      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </TouchableOpacity>
       </View>
 
       
