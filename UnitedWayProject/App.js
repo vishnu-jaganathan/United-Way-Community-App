44e5d8b21525780a53ba7c4261fbbead248f30e0
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2018-06-19 09:27:17 -0500

    recording and question buttons

--- UnitedWayProject/App.js
+++ UnitedWayProject/App.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Header, StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Header
-        leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
-      />
+      <View style={{justifyContent: 'space-between', height: 500}}>
 
-      <View style={styles.container}>
-        <Text>United Way of Chicago's Storytelling</Text>
+      <View style={{justifyContent: 'flex-end', width: 400, height: 50, backgroundColor: 'lightblue'}}>
+      <Text style={{fontWeight:'bold', fontSize: 25}}>Record Interview Questions</Text>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 1</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 2</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 3</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 4</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 5</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 6</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      <View style={{justifyContent: 'space-between', width: 360, height: 50, backgroundColor: 'orange', 
+      flexDirection: 'row'}}>
+      <Text style={{fontSize: 20}}>Question 7</Text>
+      <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
+      </View>
+
+      
+
       </View>
     );
   }
