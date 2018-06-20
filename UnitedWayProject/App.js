b3f8c5261e851a6149563776037e710b76973b7f
Jennifer Nguyen <jennifernguyenx3@gmail.com>
2018-06-20 14:21:49 -0500

    added voice recording screen

--- UnitedWayProject/App.js
+++ UnitedWayProject/App.js
@@ -1,23 +1,86 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {AppRegistry, View, ImageBackground, StyleSheet, Text, Image, Button, ImageButton, Alert, TouchableOpacity  } from 'react-native';
 
-export default class App extends React.Component {
+
+
+export default class DisplayAnImage extends Component {
+  _onPressButton() {Alert.alert('button works')}
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      
+       
+       
+       <ImageBackground
+          style= {styles.container}
+          source={require('./watermark.png')} >
+         <View style={{justifyContent:'center'}}>
+
+        <Text style={styles.text}> Question goes here</Text>
+        
+      
+        <View style={{alignSelf: 'stretch', flexDirection:'row', justifyContent: 'space-between', flex:1}}>
+      <TouchableOpacity style={{width:80, height: 80 }} onPress={this._onPressButton}>
+          <Image
+         style= {styles.playButton}
+         source={require('./play.png')} />
+         </TouchableOpacity>
+         
+         <TouchableOpacity style={{width:80, height: 80  }} onPress={this._onPressButton}>
+         <Image
+         style= {styles.recordButton}
+         source={require('./record.png')} />
+         </TouchableOpacity>
+        
+          <TouchableOpacity style={{width:80, height: 80 }} onPress={this._onPressButton}>
+         <Image
+         style= {styles.stopButton}
+         source={require('./stop.png')} />
+         </TouchableOpacity>
+         </View>
+         </View>
+  
+        </ImageBackground>
+       
+
+        
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:1,
+    width: '100%',
+    height:'100%',
     justifyContent: 'center',
+
   },
+  text:{
+    textAlign: 'center',
+    fontSize: 40,
+    color: 'black',
+    fontFamily: 'Roboto',
+    
+  },
+  playButton: {
+     height: 80,
+     width: 80,
+
+     
+     
+  }, 
+  recordButton: {
+    height: 80,
+    width: 80,
+ 
+    
+   
+
+  },
+  stopButton: {
+    height: 80,
+     width: 80,
+    
+  }
+
 });
