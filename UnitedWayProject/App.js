bf325ee248ac1428428c55a2546b757460bf8b4b
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2018-06-25 10:48:08 -0500

    matched right questions to question buttons to be recorded

--- UnitedWayProject/App.js
+++ UnitedWayProject/App.js
@@ -25,7 +25,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 1</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture') }>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'Tell me a little about yourself. This can include information about your job, your hobbies, your family, or anything else you choose.'}) }>
         <Image source ={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -33,7 +34,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 2</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'How long have you lived in your neighborhood, and what do you like best about living here?'}) }>
         <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -41,7 +43,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 3</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'What do you think are the biggest challenges facing your neighborhood today, and what do you think are the best solutions to those challenges?'}) }>
         <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -49,7 +52,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 4</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'What do you believe makes a community great?'}) }>
         <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -57,7 +61,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 5</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'What are you most proud of?'}) }>
         <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -65,7 +70,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 6</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'What are your hopes for yourself and for your community?'}) }>
        <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -73,7 +79,8 @@ class Recordings extends React.Component {
       <View style={{justifyContent: 'space-between', alignSelf: 'stretch', height: 50, backgroundColor: 'orange', 
       flexDirection: 'row'}}>
       <Text style={{fontSize: 20}}>Question 7</Text>
-      <TouchableOpacity style={{width: 50, height: 50}} onPress={this._onPressButton}>
+      <TouchableOpacity style={{width: 50, height: 50}} onPress={ ()=> navigate('VoiceCapture', 
+      {question: 'Is there anything else you’d like to tell me?'}) }>
         <Image source={require('./mic_icon.png')} style={{width: 50, height: 50}}/>
       </TouchableOpacity>
       </View>
@@ -103,8 +110,8 @@ class VoiceCapture extends React.Component {
           style= {styles.container}
           source={require('./background.png')} >
          <View style={{justifyContent:'center'}}>
-
-        <Text style={styles.text}> Question goes here</Text>
+    
+        <Text style={styles.text}>{this.props.navigation.state.params.question}</Text>
         
       
         <View style={{alignSelf: 'stretch', flexDirection:'row', justifyContent: 'space-between', flex:1}}>
@@ -156,7 +163,7 @@ const styles = StyleSheet.create({
   },
   text:{
     textAlign: 'center',
-    fontSize: 40,
+    fontSize: 20,
     color: 'black',
     fontFamily: 'Roboto',
     
